9827e95ff07fcc8afeff10c12656a5dc5fa5032d
talia <talia.van.den.bossche@student.howest.be>
2023-09-27 15:23:40 +0200

    voor dark mode

--- script/app.js
+++ script/app.js
@@ -1,5 +1,15 @@
 const handlePasswordSwitcher = function () {
     console.log("handlePasswordSwitcher executed");
+    const jsPassword = document.querySelector('.js-password-input')
+    const jsToggle = document.querySelector('.js-password-toggle-checkbox');
+    jsToggle.addEventListener('click',function () { 
+        if(jsPassword.type == 'text'){
+            jsPassword.type = 'password';
+        }
+        else if(jsPassword.type == 'password'){
+            jsPassword.type = 'text';
+        }
+    })
 }
 
 const init = function () {
